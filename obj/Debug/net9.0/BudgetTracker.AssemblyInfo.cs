ddd2b6b37ae53a1b2321a30b94fbaefeb9c6f1ea
Tyler <Tyler_Scherrer00@yahoo.com>
2024-12-14 20:44:12 -0600

    Updated UI for Set Budget, Add Budget button

--- obj/Debug/net9.0/BudgetTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/BudgetTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd52e70b1c24e81ccb1d6c03ba8e4180cf33ce3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+227f76cc18418cfffc3c7be78faa4f8b1f10f130")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
