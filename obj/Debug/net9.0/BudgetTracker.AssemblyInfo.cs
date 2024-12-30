9007dbf97987a277f356d2ad697c0e6fa0417230
Tyler <Tyler_Scherrer00@yahoo.com>
2024-12-30 01:28:43 -0600

    UI Updates

--- obj/Debug/net9.0/BudgetTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/BudgetTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f939369107399c9541bd5395dac500b91ce66a1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39cfb3b5e5989828125454d8084082f952dd8f77")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
