092bbfab0436e89aa947583349b7972fb51fbeae
Tyler <Tyler_Scherrer00@yahoo.com>
2025-01-17 23:02:48 -0600

    documentation

--- FinanceMVCApp.Tests/obj/Debug/net9.0/FinanceMVCApp.Tests.AssemblyInfo.cs
+++ FinanceMVCApp.Tests/obj/Debug/net9.0/FinanceMVCApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceMVCApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a831688c4e813cbff8af1a4242d6ecac007840")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee80b524c76736d4b796f33e74e3f622c4dc9a4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceMVCApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceMVCApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
